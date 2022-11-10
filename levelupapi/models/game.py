e77d72c2c3c06d1285bed0b951c7b09952588764
Faith Cox <fcox662@gmail.com>
2022-11-09 22:48:51 -0500

    game form and game fetches

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Game(models.Model):
 
-    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE, related_name='game_types', null=True, blank=True)
+    game_type = models.ForeignKey("GameType", on_delete=models.CASCADE, related_name='game_type', null=True, blank=True)
     title = models.CharField(max_length=50)
     gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name='gamers', null=True, blank=True)
     number_of_players = models.IntegerField()
--- levelupapi/views/game_view.py
+++ levelupapi/views/game_view.py
@@ -37,17 +37,19 @@ class GameView(ViewSet):
         Returns
             Response -- JSON serialized game instance
         """
-        new_game = Game()
-        new_game.gamer = Gamer.objects.get(user=request.auth.user)
-        new_game.game_type = GameType.objects.get(pk=request.data['game_type'])
-        new_game.title = request.data['title']
-        new_game.number_of_players = request.data['number_of_players']
-        new_game.skill_level = request.data['skill_level']
-        new_game.maker = request.data['maker']
-        new_game.save()
+        gamer = Gamer.objects.get(user=request.auth.user)
+        game_type = GameType.objects.get(pk=request.data["game_type"])
 
-        serialized = GameSerializer(new_game, many=False)
-
+        game = Game.objects.create(
+            gamer=gamer,
+            game_type=game_type,
+            title=request.data["title"],
+            number_of_players=request.data["number_of_players"],
+            skill_level=request.data["skill_level"],
+            maker=request.data["maker"]
+        )
+        
+        serialized = GameSerializer(game)
         return Response(serialized.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, pk=None):
@@ -56,20 +58,19 @@ class GameView(ViewSet):
         Returns:
             Response -- Empty body with 204 status code
         """
+        game_type = GameType.objects.get(pk=request.data["game_type"])
 
         game = Game.objects.get(pk=pk)
-        game.title = request.data['title']
-        game.maker = request.data['maker']
-        game.number_of_players = request.data['number_of_players']
-        game.skill_level = request.data['skill_level']
-
-        game_type = GameType.objects.get(pk=request.data['game_type'])
+        game.title = request.data["title"]
+        game.maker = request.data["maker"]
+        game.number_of_players = request.data["number_of_players"]
+        game.skill_level = request.data["skill_level"]
         game.game_type = game_type
         game.save()
 
         return Response(None, status=status.HTTP_200_OK)
 
-    def destroy(self, request, pk):
+    def destroy(self, request, pk=None):
         game = Game.objects.get(pk=pk)
         game.delete()
         return Response(None, status=status.HTTP_204_NO_CONTENT)
